49f95b18823889c97efd3380f613e2f418a00634
badre429 <badre429@hotmail.com>
2021-05-31 11:54:43 +0100

    wip fix build

---
+++ build.ps1
@@ -0,0 +1,7 @@
+
+#  Expand-Archive -LiteralPath .\context-setup-win64.zip
+
+#  .\context-setup-win64\context\bin\luatex.exe -synctex=1 -interaction=nonstopmode --shell-escape .\index.tex
+#  & .\context-setup-win64\context\first-setup.bat
+
+luatex.exe  -synctex=1 -interaction=nonstopmode --shell-escape .\spiral.tex

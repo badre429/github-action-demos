b61c4b034f3f1305f0615f891039a0da647f3dda
badre429 <badre429@hotmail.com>
2021-05-31 12:51:54 +0100

    Run a powershell script

--- build.ps1
+++ build.ps1
@@ -4,5 +4,6 @@
 #  .\context-setup-win64\context\bin\luatex.exe -synctex=1 -interaction=nonstopmode --shell-escape .\index.tex
 #  & .\context-setup-win64\context\first-setup.bat
 
-luatex.exe  -synctex=1 -interaction=nonstopmode --shell-escape   --enable-installer  .\spiral.tex 
-dir
+Get-ChildItem
+luatex.exe  --synctex=1 --interaction=batchmode  --shell-escape --enable-installer  .\spiral.tex
+Get-ChildItem
